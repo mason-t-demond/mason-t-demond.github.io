60f4a05ab785efa213c84b45f9078c68490c0077
mason-pAoLo <masonmillionaire725@gmail.com>
2023-09-13 05:12:13 -0500

    lab 4's core complete

---
+++ csci340lab4/script.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+    $('#genre-btn').click(function() {
+      $.ajax({
+        dataType: "json",
+        url: "https://binaryjazz.us/wp-json/genrenator/v1/genre/",
+        success: function(results) {
+          console.log(results);
+          $('#genre').text(results)
+        },
+        error: function(xhr,status,error) {
+          console.log(error);
+        }
+      });
+    });
+  });
+
+$('#mix-btn').click(function() {
+// Check for present genre
+if ($('#genre').text() == "") {
+    console.log('Genre has not been generated');
+    $('#songs').text("Please generate a genre first!!!");
+} 
+else {
+    // put genre into a proper search term (https://stackoverflow.com/a/1983661)
+    var str = $('#genre').text();
+    str = str.replace(/\s+/g, '-'); 
+
+    // Get search contents (https://publicapis.io/i-tunes-search-api)
+    const url = "https://justcors.com/l_anq6dsiw2hi/itunes.apple.com/search?key=mix&term=" + str;
+    console.log(url);
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+    // Handle the response data here
+    console.log(data);
+    // check if there are generated results
+    if (data["resultCount"] == 0) {
+        $('#songs').text("Your genre is a bit too niche. Try generating something else!");
+    } else {
+        var track_count = Math.min(12, data["resultCount"]);
+
+        $('#songs').text("");
+        for (var i = 0; i < track_count; i++) {
+            var song_anchor = $("<a>").attr("href", data["results"][i]["trackViewUrl"])
+            .text(data["results"][i]["artistName"] + " - " + data["results"][i]["trackCensoredName"]);
+            $('#songs').append(song_anchor)
+            $('#songs').append("<br>")
+        }
+    }
+    })
+    .catch(error => {
+    // Handle the error here
+    console.error(error);
+    });
+}})
