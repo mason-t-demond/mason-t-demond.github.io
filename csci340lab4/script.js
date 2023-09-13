a13dce793b4582230cc8d9f1f818defd4c2272a9
mason-pAoLo <masonmillionaire725@gmail.com>
2023-09-13 06:25:23 -0500

    changed website style

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -23,10 +23,14 @@ if ($('#genre').text() == "") {
 else {
     // put genre into a proper search term (https://stackoverflow.com/a/1983661)
     var str = $('#genre').text();
-    str = str.replace(/\s+/g, '-'); 
+
+    // replace spaces, forward slashes and dashes
+    str = str.replace(/\s+/g, '+');
+    str = str.replace(/\//g, '+'); 
+    str = str.replace(/-/g, '+'); 
 
     // Get search contents (https://publicapis.io/i-tunes-search-api)
-    const url = "https://justcors.com/l_anq6dsiw2hi/itunes.apple.com/search?key=mix&term=" + str;
+    const url = "https://justcors.com/l_anq6dsiw2hi/itunes.apple.com/search?key=mix&term=" + str + "&limit=15";
     console.log(url);
 
     fetch(url)
@@ -38,15 +42,14 @@ else {
     if (data["resultCount"] == 0) {
         $('#songs').text("Your genre is a bit too niche. Try generating something else!");
     } else {
-        var track_count = Math.min(12, data["resultCount"]);
-
         $('#songs').text("");
-        for (var i = 0; i < track_count; i++) {
+        for (var i = 0; i < data["resultCount"]; i++) {
             var song_anchor = $("<a>").attr("href", data["results"][i]["trackViewUrl"])
             .text(data["results"][i]["artistName"] + " - " + data["results"][i]["trackCensoredName"]);
             $('#songs').append(song_anchor)
             $('#songs').append("<br>")
         }
+        $('#genre-cover').attr("src", data["results"][0]["artworkUrl100"])
     }
     })
     .catch(error => {
